cc2559ebc83f356daee3f40dabb1e8331fcdd527
Aniket Bharane <codeaniket123@gmail.com>
2024-06-11 10:17:01 +0530

    sixth commit

--- pages/components/Done.js
+++ pages/components/Done.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './Navbar'
 import donestyle from "../../styles/donestyle.module.css";
+import Image from 'next/image';
 
 function Done() {
   return (
@@ -9,6 +10,10 @@ function Done() {
     <div className={donestyle.main} >
       <h1>Your ride has been booked</h1>
     </div>
+    
+      <div  className={donestyle.img} >
+        <Image width={200} height={150} src={"/tick.png"} ></Image>
+      </div>
     </>
   )
 }
